10e053887dd203d648a24870f6de6a0ddc287c18
Delfi1 <fezdx@mail.ru>
2023-12-03 18:32:08 +0300

    Add Scenes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #[macro_use]
 extern crate glium;
-extern crate winit;
-
 use glium::{
     backend::glutin::SimpleWindowBuilder,
     Surface
 };
-use objects::Object3D;
+
+extern crate winit;
+use scenes::Camera;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoop
@@ -32,6 +32,12 @@ fn main() {
 
     _window.set_min_inner_size(Some(winit::dpi::LogicalSize::new(350.0, 250.0)));
 
+    let global_camera = Camera::new([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], 1.0, 1.0, 1.0);
+
+    let mut test_scene = scenes::Scene::new(global_camera);
+
+    scenes::Cube::new(&mut test_scene, [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 1.0, 1.0]);
+
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll();
         control_flow.set_wait();
@@ -50,38 +56,10 @@ fn main() {
             Event::RedrawRequested(_) => {
                 // Отрисовка кадра
                 let mut frame = _display.draw();
-                frame.clear_color_and_depth(rgba_to_float(18, 18, 24, 1.0), 1.0);
+                frame.clear_color_and_depth(rgba_to_float(22, 22, 29, 1.0), 0.8);
 
-                let perspective = {
-                    let (width, height) = frame.get_dimensions();
-                    let aspect_ratio = height as f32 / width as f32;
-
-                    let fov: f32 = 3.141592 / 3.0;
-                    let zfar = 1024.0;
-                    let znear = 0.1;
-
-                    let f = 1.0 / (fov / 2.0).tan();
-
-                    [
-                        [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
-                        [         0.0         ,     f ,              0.0              ,   0.0],
-                        [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
-                        [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
-                    ]
-                };
-
-                let global_light = [-1.0, 0.4, 0.9f32];
-
-                let params = glium::DrawParameters {
-                    depth: glium::Depth {
-                        test: glium::draw_parameters::DepthTest::IfLess,
-                        write: true,
-                        .. Default::default()
-                    },
-                    //backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
-                    .. Default::default()
-                };
-
+                test_scene.render();
+                
                 // Окончание отрисовки кадра
                 frame.finish().unwrap();
             },
--- src/scenes.rs
+++ src/scenes.rs
@@ -1,19 +1,110 @@
-#[path = "../src/objects.rs"]
-mod objects;
+#[path = "../src/teapot.rs"]
+mod teapot;
 
-use objects::Object3D;
+use glium::{Surface, glutin::{display, surface::WindowSurface}, DrawParameters, GlObject};
 
-#[derive(Default)]
-struct Scene {
-    objects: Vec<dyn Object3D>
+#[derive(Clone, Copy)]
+struct Vertex {
+    position: [f32; 3],
+    normal: [f32; 3],
+}
+implement_vertex!(Vertex, position, normal);
+
+pub struct Scene {
+    camera: Camera,
+    objects: Vec<Box<dyn Object3D>>
 }
 
 impl Scene {
-    pub fn new() -> Self {
-        return {..Default::default()}
+    pub fn new(camera: Camera) -> Self {
+        Scene { camera, objects: Vec::new() }
+    }
+
+    fn add_object(&mut self, object: Box<dyn Object3D>) {
+        self.objects.push(object);
+    }
+
+    pub fn get_object(&self, id: i32) -> Result<&Box<dyn Object3D>, String> {
+        for object in &self.objects {
+            if object.get_id() == id {
+                return Ok(&object);
+            }
+        }
+        Err("Object not found".to_string())
     }
 
     pub fn render(&self) {
-        self.objects.iter().render();
+        for object in &self.objects {
+            object.render(&self);
+        }
+    }
+}
+
+pub trait Object3D {    
+    fn get_id(&self) -> i32;
+
+    fn get_position(&self) -> [f32; 3];
+    fn set_position(&mut self, position: [f32; 3]);
+
+    fn get_rotation(&self) -> [f32; 3];
+    fn set_rotation(&mut self, rotation: [f32; 3]);   
+
+    fn render(&self, parent_scene: &Scene);
+}
+
+pub struct Camera {
+    position: [f32; 3],
+    direction: [f32; 3],
+    up: [f32; 3],
+
+    fov: f32,
+    zfar: f32,
+    znear: f32
+}
+
+impl Camera {
+    pub fn new(position: [f32; 3], direction: [f32; 3], up: [f32; 3], fov: f32, zfar: f32, znear: f32) -> Self {
+        Camera { position, direction, up, fov, zfar, znear }
+    }
+}
+
+pub struct Cube {
+    id: i32,
+
+    position: [f32; 3],
+    rotation: [f32; 3],
+    size: [f32; 3]
+}
+
+impl Cube {
+    pub fn new(scene: &mut Scene, position: [f32; 3], rotation: [f32; 3], size: [f32; 3]) {
+        let _id = scene.objects.len() as i32;
+
+        let cube = Cube {id: _id, position, rotation, size};
+
+        scene.add_object(Box::new(cube));
+    }
+}
+
+impl Object3D for Cube {
+    fn get_id(&self) -> i32 {
+        self.id
+    }
+    fn get_position(&self) -> [f32; 3] {
+        self.position
+    }
+    fn get_rotation(&self) -> [f32; 3] {
+        self.rotation
+    }
+
+    fn set_position(&mut self, position: [f32; 3]) {
+        self.position = position
+    }
+    fn set_rotation(&mut self, rotation: [f32; 3]) {
+        self.rotation = rotation
+    }
+
+    fn render(&self, parent_scene: &Scene) {
+        println!("Test");
     }
 }
