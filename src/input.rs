93cd5a1c09dddab51cdb9c8423239143721f4fba
Delfi1 <fezdx@mail.ru>
2023-12-09 20:44:58 +0300

    Add Key binding

---
+++ src/input.rs
@@ -0,0 +1,40 @@
+use std::{time::Instant, default};
+
+use winit::event::{Event, WindowEvent, KeyboardInput, VirtualKeyCode, ElementState, };
+
+pub struct Key {
+    last_press: Instant,
+    culldown: f32,
+    virtual_keycode: VirtualKeyCode
+}
+
+impl Key {
+    pub fn new(culldown: f32, virtual_keycode: VirtualKeyCode) -> Self {
+        let last_press = Instant::now();
+        Self {last_press, culldown, virtual_keycode}
+    }
+
+    pub fn check_event(&mut self, event: &Event<'_, ()>) -> bool {
+        if Instant::now().duration_since(self.last_press).as_secs_f32() < self.culldown {
+            return false;
+        }
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::KeyboardInput {
+                    device_id, 
+                    input: KeyboardInput {virtual_keycode, state, ..},
+                    is_synthetic 
+                },
+                ..
+            } => {
+                if *virtual_keycode == Some(self.virtual_keycode.clone()) && *state == ElementState::Pressed  {
+                    self.last_press = Instant::now();
+                    return true;
+                }
+            },
+            _ => ()
+        }
+        return false;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,13 @@ use glium::{
 
 extern crate winit;
 use winit::{
-    event::{Event, WindowEvent},
+    event::{Event, WindowEvent, KeyboardInput, VirtualKeyCode},
     event_loop::{EventLoop, ControlFlow, self, EventLoopBuilder},
-    window::{Window, self}, dpi::PhysicalSize
+    window::{Window, self, Fullscreen}, dpi::PhysicalSize, monitor::{MonitorHandle, VideoMode}
 };
+
+mod input;
+use input::Key;
 
 const TARGET_FPS: u64 = 120;
 
@@ -21,18 +24,35 @@ fn main() {
     // Основной цикл для программы
     let event_loop = EventLoop::new();
     // Создаем базовое окно.
-    let (mut window, display) = SimpleWindowBuilder::new().build(&event_loop);
+    let (window, display) = SimpleWindowBuilder::new().build(&event_loop);
 
     // Настраиваем Окно
     window.set_title("DEngine");
     window.set_inner_size(PhysicalSize::new(700, 500)); // Размер окна
     window.set_min_inner_size(Some(PhysicalSize::new(350, 250))); // Минимальный размер окна
 
+    // Кнопки
+    let mut f11_bind = Key::new(0.5, VirtualKeyCode::F11);
+    let mut f3_bind = Key::new(0.5, VirtualKeyCode::F3);
+
     // Запускаем основной цикл.
-    event_loop.run(move |event, _, control_flow| {
+    event_loop.run(move |event, _target, control_flow| {
         let start_time = Instant::now(); 
         control_flow.set_poll(); 
         control_flow.set_wait();
+
+        if f11_bind.check_event(&event) {
+            if window.fullscreen().is_none() {
+                let mode = window.current_monitor().unwrap().video_modes().next().unwrap();
+                window.set_fullscreen(Some(Fullscreen::Exclusive(mode)));
+            } else {
+                window.set_fullscreen(None);
+            }
+        }
+
+        if f3_bind.check_event(&event) {
+            println!("OpenGL version: {:?}", display.get_opengl_version_string());
+        }
 
         match event {
             Event::WindowEvent {
@@ -40,12 +60,16 @@ fn main() {
                 ..
             } => {
                 println!("Закрытие окна...");
-                control_flow.set_exit();
+                control_flow.set_exit_with_code(0);
             },
+            
+            
             // Отрисовка окна
             Event::RedrawRequested(_) => {
+                let mut frame = display.draw(); // Отрисовка кадра
+                frame.clear_color(22.0/255.0, 22.0/255.0, 29.0/255.0, 1.0);
 
-
+                frame.finish().unwrap();
             },
             _ => ()
         };
@@ -61,7 +85,7 @@ fn main() {
                     true => 1_000_000_000 / TARGET_FPS - elapsed_time,
                     false => 0
                 };
-                
+                //println!("{}", wait_nanos);
                 let new_inst = start_time + std::time::Duration::from_nanos(wait_nanos);
                 *control_flow = ControlFlow::WaitUntil(new_inst); // Ожидание в наносекундах.
             }
